c2cfde49866c9c829b5c2916fef4a5eaa272a66d
Moooover <kevin.cheng319@gmail.com>
2023-03-02 13:08:06 +0100

    boostagram builder

--- src/lib.rs
+++ src/lib.rs
@@ -4,30 +4,26 @@ use thiserror::Error;
 
 use base64::{Engine as _, engine::general_purpose};
 
+pub type Result<T> = std::result::Result<T, Error>;
+
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("serde_json error: {0}")]
-    Deserializing(String),
+    DeserializingError(#[from] serde_json::Error),
+
+    #[error("b64 decoding error: {0}")]
+    B64DecodeError(#[from] base64::DecodeError),
+
+    #[error("buidling error: {0}")]
+    BuildingError(String),
 }
 
-impl From<serde_json::Error> for Error {
-    fn from(err: serde_json::Error) -> Self {
-        Error::Deserializing(err.to_string())
-    }
-}
-
-impl From<base64::DecodeError> for Error {
-    fn from(err: base64::DecodeError) -> Self {
-        Error::Deserializing(err.to_string())
-    }
-}
-
-pub fn from_b64(b64: &str) -> Result<Boostagram, Error> {
+pub fn from_b64(b64: &str) -> Result<Boostagram> {
     let json_raw = general_purpose::STANDARD.decode(b64)?;
     serde_json::from_slice(&json_raw).map_err(Error::from)
 }
 
-pub fn from_json(json: &str) -> Result<Boostagram, Error> {
+pub fn from_json(json: &str) -> Result<Boostagram> {
     serde_json::from_str(json).map_err(Error::from)
 }
 
@@ -100,4 +96,189 @@ pub struct Boostagram {
 
     //can't be larger for the lightning network
     pub value_msat_total: Option<u64>,
+}
+
+pub struct BoostagramBuilder {
+    boostagram: Boostagram,
+}
+
+impl BoostagramBuilder {
+
+    pub fn build(self) -> Result<Boostagram> {
+        let boost = self.boostagram;
+
+        if boost.podcast.is_none() || boost.feed_id.is_none() || boost.url.is_none() || boost.guid.is_none() {
+            return Err(Error::BuildingError("either podcast, feed_id, url or guid must be set according to spec".to_string()))
+        }
+
+        Ok(boost)
+    }
+
+    pub fn new() -> BoostagramBuilder {
+        BoostagramBuilder {
+            boostagram: Boostagram {
+                podcast: None,
+                feed_id: None,
+                url: None,
+                guid: None,
+                episode: None,
+                item_id: None,
+                episode_guid: None,
+                time: None,
+                ts: None,
+                action: None,
+                app_name: None,
+                app_version: None,
+                boost_link: None,
+                message: None,
+                name: None,
+                pubkey: None,
+                seconds_back: None,
+                sender_key: None,
+                sender_name: None,
+                sender_id: None,
+                sig_fields: None,
+                signature: None,
+                speed: None,
+                uuid: None,
+                value_msat: None,
+                value_msat_total: None,
+            },
+        }
+    }
+
+    pub fn podcast(mut self, podcast: String) -> Self {
+        self.boostagram.podcast = Some(podcast);
+        self
+    }
+    
+    pub fn amount(mut self, amount: u64) -> Self {
+        self.boostagram.value_msat = Some(amount);
+        self
+    }
+
+    pub fn feed_id(mut self, feed_id: usize) -> Self {
+        self.boostagram.feed_id = Some(feed_id);
+        self
+    }
+
+    pub fn url(mut self, url: String) -> Self {
+        self.boostagram.url = Some(url);
+        self
+    }
+
+    pub fn guid(mut self, guid: String) -> Self {
+        self.boostagram.guid = Some(guid);
+        self
+    }
+
+    pub fn episode(mut self, episode: String) -> Self {
+        self.boostagram.episode = Some(episode);
+        self
+    }
+
+    pub fn item_id(mut self, item_id: usize) -> Self {
+        self.boostagram.item_id = Some(item_id);
+        self
+    }
+
+    pub fn message(mut self, message: String) -> Self {
+        self.boostagram.message = Some(message);
+        self
+    }
+
+    pub fn name(mut self, name: String) -> Self {
+        self.boostagram.name = Some(name);
+        self
+    }
+
+    pub fn pubkey(mut self, pubkey: String) -> Self {
+        self.boostagram.pubkey = Some(pubkey);
+        self
+    }
+
+    pub fn seconds_back(mut self, seconds_back: usize) -> Self {
+        self.boostagram.seconds_back = Some(seconds_back);
+        self
+    }
+
+    pub fn sender_key(mut self, sender_key: String) -> Self {
+        self.boostagram.sender_key = Some(sender_key);
+        self
+    }
+
+    pub fn sender_name(mut self, sender_name: String) -> Self {
+        self.boostagram.sender_name = Some(sender_name);
+        self
+    }
+
+    pub fn sender_id(mut self, sender_id: String) -> Self {
+        self.boostagram.sender_id = Some(sender_id);
+        self
+    }
+
+    pub fn sig_fields(mut self, sig_fields: String) -> Self {
+        self.boostagram.sig_fields = Some(sig_fields);
+        self
+    }
+
+    pub fn signature(mut self, signature: String) -> Self {
+        self.boostagram.signature = Some(signature);
+        self
+    }
+
+    pub fn speed(mut self, speed: String) -> Self {
+        self.boostagram.speed = Some(speed);
+        self
+    }
+
+    pub fn uuid(mut self, uuid: String) -> Self {
+        self.boostagram.uuid = Some(uuid);
+        self
+    }
+
+    pub fn value_msat(mut self, value_msat: u64) -> Self {
+        self.boostagram.value_msat = Some(value_msat);
+        self
+    }
+
+    pub fn value_msat_total(mut self, value_msat_total: u64) -> Self {
+        self.boostagram.value_msat_total = Some(value_msat_total);
+        self
+    }
+
+    pub fn episode_guid(mut self, episode_guid: String) -> Self {
+        self.boostagram.episode_guid = Some(episode_guid);
+        self
+    }
+
+    pub fn time(mut self, time: String) -> Self {
+        self.boostagram.time = Some(time);
+        self
+    }
+
+    pub fn ts(mut self, ts: usize) -> Self {
+        self.boostagram.ts = Some(ts);
+        self
+    }
+
+    pub fn action(mut self, action: Action) -> Self {
+        self.boostagram.action = Some(action);
+        self
+    }
+
+    pub fn app_name(mut self, app_name: String) -> Self {
+        self.boostagram.app_name = Some(app_name);
+        self
+    }
+
+    pub fn app_version(mut self, app_version: String) -> Self {
+        self.boostagram.app_version = Some(app_version);
+        self
+    }
+
+    pub fn boost_link(mut self, boost_link: String) -> Self {
+        self.boostagram.boost_link = Some(boost_link);
+        self
+    }
 }
